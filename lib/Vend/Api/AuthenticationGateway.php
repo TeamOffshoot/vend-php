aba59276f409990e5d13d4c33ce8f771a5146a27
Chris Woodford <chris.woodford@gmail.com>
2014-12-12 08:57:24 -0800

    update the authentication gateway to pass the first test

--- lib/Vend/Api/AuthenticationGateway.php
+++ lib/Vend/Api/AuthenticationGateway.php
@@ -8,11 +8,11 @@ use Offshoot\Redirector;
 class AuthenticationGateway
 {
 
-    const AUTHORIZATION_URI = 'https://secure.vendhq.com/connect?response_type=code&client_id=%s&redirect_uri=%s';
+    const AUTHORIZATION_URI = 'https://secure.vendhq.com/connect';
     const ACCESS_URI = 'https://%s.vendhq.com/api/1.0/token';
 
     /** @var string */
-    protected $shopName;
+    protected $storeName;
 
     /** @var string */
     protected $clientId;
@@ -38,6 +38,159 @@ class AuthenticationGateway
     {
         $this->httpClient = $httpClient;
         $this->redirector = $redirector;
+    }
+
+    /**
+     * a simple DSL on top of setting the store name
+     * @param string $storeName
+     * @return AuthenticationGateway
+     */
+    public function forStoreName($storeName)
+    {
+        $this->setStoreName($storeName);
+        return $this;
+    }
+
+    /**
+     * a simple DSL on top of setting the client ID
+     * @param string $clientId
+     * @return AuthenticationGateway
+     */
+    public function usingClientId($clientId)
+    {
+        $this->setClientId($clientId);
+        return $this;
+    }
+
+    /**
+     * a simple DSL on top of setting the redirect URI
+     * @param string $redirectUri
+     * @return AuthenticationGateway
+     */
+    public function andReturningTo($redirectUri)
+    {
+        $this->setRedirectUri($redirectUri);
+        return $this;
+    }
+
+    /**
+     * initiate the login process
+     */
+    public function initiateLogin()
+    {
+
+        if (!$this->canInitiateLogin()) {
+            throw new \RuntimeException(
+                'Unable to initiate login'
+            );
+        }
+
+        $uri = $this->getAuthenticationUri();
+        return $this->redirector->redirect($uri);
+
+    }
+
+    /**
+     * build the uri that users are forwarded to for authentication
+     * @return string
+     */
+    public function getAuthenticationUri()
+    {
+
+        $authorizeUri = self::AUTHORIZATION_URI;
+
+        $uriParams = array(
+            'response_type' => 'code',
+            'client_id' => $this->getClientId()
+        );
+
+        if ($this->getRedirectUri()) {
+            $uriParams['redirect_uri'] = $this->getRedirectUri();
+        }
+
+        return $authorizeUri . '?' . http_build_query($uriParams);
+
+    }
+
+    /**
+     * assert that it is possible to proceed with initiating the login
+     * @return boolean
+     */
+    protected function canInitiateLogin()
+    {
+
+        if (!$this->canBuildAuthenticationUri()) {
+            throw new \RuntimeException(
+                'Cannot build authentication uri, dependencies are missing'
+            );
+        }
+
+        return true;
+
+    }
+
+    /**
+     * assert that it is possible to build the authentication uri
+     * @return boolean
+     */
+    protected function canBuildAuthenticationUri()
+    {
+        return $this->getClientId()
+            && $this->getStoreName();
+    }
+
+    /**
+     * set the store name
+     * @param string $storeName
+     */
+    protected function setStoreName($storeName)
+    {
+        $this->storeName = $storeName;
+    }
+
+    /**
+     * get the store name
+     * @return string
+     */
+    protected function getStoreName()
+    {
+        return $this->storeName;
+    }
+
+    /**
+     * set the client ID
+     * @param string $clientId
+     */
+    protected function setClientId($clientId)
+    {
+        $this->clientId = $clientId;
+    }
+
+    /**
+     * get the client ID
+     * @return string
+     */
+    protected function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * set the redirect URI
+     * @param string $redirectUri
+     */
+    protected function setRedirectUri($redirectUri)
+    {
+        $this->redirectUri = $redirectUri;
+    }
+
+    /**
+     * get the redirect uri
+     * @return string
+     */
+    protected function getRedirectUri()
+    {
+        return $this->redirectUri;
     }
 
 }
